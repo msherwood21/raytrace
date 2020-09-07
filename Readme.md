0f6997a390e8d9a92a17900dacc8c935bd46640d
Matthew Sherwood <msherwood21@gmail.com>
2020-09-06 20:17:54 -0500

    Initial commit. Readme and license added. Completed code up to section 2.2.

---
+++ Readme.md
@@ -0,0 +1,14 @@
+Raytrace Rust
+=============
+
+## What Is This?
+The results of me going through [Ray Tracing In One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html).
+Instead of coding it in C++, I'm doing it in Rust to teach myself the language. This has already been
+done elsewhere (look [here](https://github.com/RayTracing/raytracing.github.io/wiki/Implementations-in-Other-Languages])),
+but I will not be looking at that source until I've tried my hand here.
+
+## Can I Use It?
+Check the [License](License.md) file for usage details
+
+## Can I Add To It?
+Just fork it and go on your merry way. Once I'm done learning I will not be touching the repository again.
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    // println!("Hello, world!");
+
+    //- Image
+    let image_width  : i32 = 256;
+    let image_height : i32 = 256;
+
+    //- Render
+    //-   Header
+    println!("P3\n{} {}\n255\n", image_width, image_height);
+
+    //-   Body
+    for j in (0..image_height).rev() {
+        for i in 0..image_width {
+            let r = f64::from(i) / f64::from(image_width - 1);
+            let g = f64::from(j) / f64::from(image_height - 1);
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+}
